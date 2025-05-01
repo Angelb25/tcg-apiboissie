2b78ee4d8fbd8473642600ab0e5f81fd9380315e
Angelb25 <ang66b@gmail.com>
2025-05-01 13:41:53 +0200

    gestion user

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require("express"); 
-const app = express(); 
-app.get("/", (req, res) => {  
-    res.json( 
-    {  
-    message : "Bienvenue sur l'API TCG",  
-    data : {} 
-    } 
-    ); 
-});  
-app.listen(3000, () => { 
-console.log("Serveur démarré sur http://localhost:3000"); 
-}); 
+const express = require("express");
+const app = express();
+const users = require("./modules/users");
+const fs = require("fs");
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Bienvenue sur l'API TCG",
+    data: {}
+  });
+});
+
+const users = require("./modules/users");
+
+app.post("/register", users.RegisterUser);
+app.post("/login", users.Login);
+app.get("/user", users.GetUser);
+app.post("/disconnect", users.Disconnect);
+
+
+app.listen(3000, () => {
+  console.log("Serveur démarré sur http://localhost:3000");
+});
---
+++ modules/users.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const path = require("path");
+
+const dataFilePath = path.join(__dirname, "../data/users.json");
+function RegisterUser(req, res) {
+    if (!req.body || !req.body.username || !req.body.password) {
+      return res.status(400).json({ message: "Erreur : Données manquantes", data: {} });
+    }
+  
+    const { username, password } = req.body;
+    let users = JSON.parse(fs.readFileSync(dataFilePath));
+  
+    // Vérifier si l'utilisateur existe déjà
+    if (users.find(u => u.username === username)) {
+      return res.status(409).json({ message: "Erreur : Utilisateur déjà existant", data: {} });
+    }
+  
+    const newId = users.length ? users[users.length - 1].id + 1 : 1;
+    const newUser = {
+      id: newId,
+      username,
+      password,
+      collection: [],
+      token: null
+    };
+  
+    users.push(newUser);
+    fs.writeFileSync(dataFilePath, JSON.stringify(users, null, 2));
+  
+    res.json({ message: "Utilisateur créé avec succès", data: newUser });
+  }
+  function generateToken(length = 16) {
+    return Math.random().toString(36).substring(2, 2 + length);
+  }
+  
+  function Login(req, res) {
+    if (!req.body || !req.body.username || !req.body.password) {
+      return res.status(400).json({ message: "Erreur : Données manquantes", data: {} });
+    }
+  
+    const { username, password } = req.body;
+    let users = JSON.parse(fs.readFileSync(dataFilePath));
+    let user = users.find(u => u.username === username && u.password === password);
+  
+    if (!user) {
+      return res.status(401).json({ message: "Erreur : Identifiants invalides", data: {} });
+    }
+  
+    user.token = generateToken();
+    fs.writeFileSync(dataFilePath, JSON.stringify(users, null, 2));
+  
+    res.json({ message: "Authentification réussie", data: { token: user.token } });
+  }
+  function GetUser(req, res) {
+    const token = req.query.token;
+    if (!token) {
+      return res.status(400).json({ message: "Erreur : Token manquant", data: {} });
+    }
+  
+    const users = JSON.parse(fs.readFileSync(dataFilePath));
+    const user = users.find(u => u.token === token);
+  
+    if (!user) {
+      return res.status(403).json({ message: "Erreur : Token invalide", data: {} });
+    }
+  
+    res.json({ message: "Utilisateur trouvé", data: user });
+  }
+  function Disconnect(req, res) {
+    const token = req.body.token;
+    if (!token) {
+      return res.status(400).json({ message: "Erreur : Token manquant", data: {} });
+    }
+  
+    const users = JSON.parse(fs.readFileSync(dataFilePath));
+    const user = users.find(u => u.token === token);
+  
+    if (!user) {
+      return res.status(403).json({ message: "Erreur : Token invalide", data: {} });
+    }
+  
+    user.token = null;
+    fs.writeFileSync(dataFilePath, JSON.stringify(users, null, 2));
+  
+    res.json({ message: "Déconnexion réussie", data: {} });
+  }
+  
+  module.exports = {
+    RegisterUser,
+    Login,
+    GetUser,
+    Disconnect
+  };
+  
